f26cec010293a29469adff7c7701bbff6e2b7963
akshaysb100 <akshaybavalekar100@gmail.com>
2019-11-18 15:17:14 +0530

    add: initiakishing bord array

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -x
 
 echo "Welcome Tic-Tac-Toe Program"
+
+#CONSTANTS
+MAX_BOARD_POSITION=9
+
+#ARRAYS
+declare -a playerBoardPosition
+
+function initialisingBoard()
+{
+	for (( i=1; i<=$MAX_BOARD_POSITION; i++ ))
+	do
+		playerBoardPosition[$i]=0
+	done
+}
+initialisingBoard
